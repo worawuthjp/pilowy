d0979583546ac598fa58432c73032c865a19bc02
parin1998 <60882177+parin1998@users.noreply.github.com>
2020-07-01 20:34:57 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <?php include './partials/head.html'?>
-    <title>test pilowy</title>
+    <title>test pilowy eieiei</title>
 </head>
 <body>
 <?php include './partials/navbar.php'; ?>
