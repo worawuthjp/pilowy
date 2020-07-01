d39a9ce61cedaa0cf42511cdcc3d074756d3371a
parin1998 <60882177+parin1998@users.noreply.github.com>
2020-07-01 21:29:55 +0700

    add facebook sdk

--- index.php
+++ index.php
@@ -1,44 +1,47 @@
 <!doctype html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <?php include './partials/head.html'?>
-    <title>test pilowy</title>
-</head>
-<body>
-<?php include './partials/navbar.php'; ?>
-<main class="container mt-5">
-    <h1 class="font-weight-bold">PILOWY WEBSITE</h1>
-</main>
 
-<?php include './partials/script.html';?>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <?php include './partials/head.html' ?>
+  <title>test pilowy eieieigi</title>
+</head>
+
+<body>
+  <?php include './partials/navbar.php'; ?>
+  <main class="container mt-5">
+    <h1 class="font-weight-bold">PILOWY WEBSITE</h1>
+  </main>
+
+  <?php include './partials/script.html'; ?>
 </body>
 <!-- Load Facebook SDK for JavaScript -->
 <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v7.0'
-          });
-        };
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml: true,
+      version: 'v7.0'
+    });
+  };
 
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s);
+    js.id = id;
+    js.src = 'https://connect.facebook.net/th_TH/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+</script>
 
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="103141214799677"
-  theme_color="#ffc300">
-      </div>
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat" attribution=setup_tool page_id="103141214799677" theme_color="#ffc300">
+</div>
+
+
+</div>
 
 </html>
