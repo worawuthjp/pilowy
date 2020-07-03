f7dda30f86b507168d16af0fd35ee85c795080e8
Worawuth Puntusithsaree <popcorns1udio@gmail.com>
2020-07-03 16:37:37 +0700

    layout by jj

--- index.php
+++ index.php
@@ -5,214 +5,219 @@ require("head.html");
 ?>
 <body class="test">
 
-    <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZNSRF2"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- Google Tag Manager (noscript) -->
+<noscript>
+    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NZNSRF2"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+</noscript>
 <!-- End Google Tag Manager (noscript) -->
 
-    <!--::header part start::-->
-    <?php
-    require("header.html");
-    ?>
-    <!-- Header part end-->
+<!--::header part start::-->
+<?php
+require("header.html");
+?>
+<!-- Header part end-->
 
-    <!-- banner part start-->
-    <section class="banner_part">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-5">
-                    <div class="banner_text">
-                        <div class="banner_text_iner">
-                            <h1>หมอนยางพาราคุณภาพสูง</h1>
-                            <p>หมอนยางพารา Pilowy เป็นหมอนยางพาราแท้ 100% จากบริษัท Thai rubber ผู้ผลิตและจำหน่ายยางพารา Top 3 ของประเทศไทย</p>
-                            <a href="product_list.php" class="btn_1">shop now</a>
+<!-- banner part start-->
+<section class="banner_part">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-5">
+                <div class="banner_text col-sm-12 pl-0 pr-0">
+                    <div class="banner_text_iner">
+                        <h1>หมอนยางพาราคุณภาพสูง</h1>
+                        <p>
+                            หมอนยางพารา Pilowy เป็นหมอนยางพาราแท้ 100% จากบริษัท Thai rubber ผู้ผลิตและจำหน่ายยางพารา Top
+                            3 ของประเทศไทย
+                        </p>
+                        <a href="product_list.php" class="btn_1">shop now</a>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="banner_img">
+        <img src="img/banner.webp" alt="#" class="img-fluid col-sm-12">
+        <!-- <img src="img/banner_pattern.png " alt="#" class="pattern_img img-fluid"> -->
+    </div>
+</section>
+<!-- banner part start-->
+
+<!-- product list start-->
+<section class="single_product_list">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="single_product_iner">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="single_product_img">
+                                <img src="img/single_product_1.webp" width="548" height="460" class="img-fluid" alt="#">
+                                <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-sm-6">
+                            <div class="single_product_content">
+                                <h5>ราคาใบละ 590 บาท</h5>
+                                <h2><a href="single-product.php">หมอนแบบ Massage </a></h2>
+                                <a href="product_list.php" class="btn_3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single_product_iner">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="single_product_img">
+                                <img src="img/single_product_2.webp" width="548" height="460" class="img-fluid" alt="#">
+                                <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-sm-6">
+                            <div class="single_product_content">
+                                <h5>ราคาใบละ 590 บาท</h5>
+                                <h2><a href="single-product.php">หมอนแบบ Contour
+                                    </a></h2>
+                                <a href="product_list.php" class="btn_3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single_product_iner">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="single_product_img">
+                                <img src="img/single_product_3.webp" width="548" height="460" class="img-fluid" alt="#">
+                                <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-sm-6">
+                            <div class="single_product_content">
+                                <h5>ราคาใบละ 590 บาท</h5>
+                                <h2><a href="single-product.php">หมอนแบบ Heart</a></h2>
+                                <a href="product_list.php" class="btn_3">Explore Now</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="single_product_iner">
+                    <div class="row align-items-center justify-content-between">
+                        <div class="col-lg-6 col-sm-6">
+                            <div class="single_product_img">
+                                <img src="img/single_product_4.webp" width="548" height="460" class="img-fluid" alt="#">
+                                <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
+                            </div>
+                        </div>
+                        <div class="col-lg-5 col-sm-6">
+                            <div class="single_product_content">
+                                <h5>ราคาใบละ 590 บาท</h5>
+                                <h2><a href="single-product.php">หมอนแบบ Durian </a></h2>
+                                <a href="product_list.php" class="btn_3">Explore Now</a>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <div class="banner_img">
-            <img src="img/banner.webp" alt="#" class="img-fluid col-sm-12">
-            <!-- <img src="img/banner_pattern.png " alt="#" class="pattern_img img-fluid"> -->
-        </div>
-    </section>
-    <!-- banner part start-->
+    </div>
+</section>
+<!-- product list end-->
 
-    <!-- product list start-->
-    <section class="single_product_list">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="single_product_iner">
-                        <div class="row align-items-center justify-content-between">
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="single_product_img">
-                                    <img src="img/single_product_1.webp" width="548" height="460" class="img-fluid" alt="#" >
-                                    <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
-                                </div>
-                            </div>
-                            <div class="col-lg-5 col-sm-6">
-                                <div class="single_product_content">
-                                    <h5>ราคาใบละ 590 บาท</h5>
-                                    <h2> <a href="single-product.php">หมอนแบบ Massage </a> </h2>
-                                    <a href="product_list.php" class="btn_3">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single_product_iner">
-                        <div class="row align-items-center justify-content-between">
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="single_product_img">
-                                    <img src="img/single_product_2.webp" width="548" height="460" class="img-fluid" alt="#">
-                                    <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
-                                </div>
-                            </div>
-                            <div class="col-lg-5 col-sm-6">
-                                <div class="single_product_content">
-                                    <h5>ราคาใบละ 590 บาท</h5>
-                                    <h2> <a href="single-product.php">หมอนแบบ Contour
-                                            </a> </h2>
-                                    <a href="product_list.php" class="btn_3">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single_product_iner">
-                        <div class="row align-items-center justify-content-between">
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="single_product_img">
-                                    <img src="img/single_product_3.webp" width="548" height="460" class="img-fluid" alt="#">
-                                    <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
-                                </div>
-                            </div>
-                            <div class="col-lg-5 col-sm-6">
-                                <div class="single_product_content">
-                                    <h5>ราคาใบละ 590 บาท</h5>
-                                    <h2> <a href="single-product.php">หมอนแบบ Heart</a> </h2>
-                                    <a href="product_list.php" class="btn_3">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="single_product_iner">
-                        <div class="row align-items-center justify-content-between">
-                            <div class="col-lg-6 col-sm-6">
-                                <div class="single_product_img">
-                                    <img src="img/single_product_4.webp" width="548" height="460" class="img-fluid" alt="#">
-                                    <img src="img/product_overlay.png" alt="#" class="product_overlay img-fluid">
-                                </div>
-                            </div>
-                            <div class="col-lg-5 col-sm-6">
-                                <div class="single_product_content">
-                                    <h5>ราคาใบละ 590 บาท</h5>
-                                    <h2> <a href="single-product.php">หมอนแบบ Durian </a> </h2>
-                                    <a href="product_list.php" class="btn_3">Explore Now</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
+
+<!-- trending item start-->
+<section class="trending_items">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="section_tittle text-center">
+                    <h2>Trending Items</h2>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- product list end-->
-
-
-    <!-- trending item start-->
-    <section class="trending_items">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <div class="section_tittle text-center">
-                        <h2>Trending Items</h2>
+        <div class="row">
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <div class="single_product_item_thumb">
+                        <img src="img/tranding_item/tranding_item_1.png" alt="#" class="img-fluid">
                     </div>
+                    <h3><a href="single-product.php">Cervical pillow for airplane
+                            car office nap pillow</a></h3>
+                    <p>From $5</p>
                 </div>
             </div>
-            <div class="row">
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <div class="single_product_item_thumb">
-                            <img src="img/tranding_item/tranding_item_1.png" alt="#" class="img-fluid">
-                        </div>
-                        <h3> <a href="single-product.php">Cervical pillow for airplane
-                                car office nap pillow</a> </h3>
-                        <p>From $5</p>
-                    </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <img src="img/tranding_item/tranding_item_2.png" alt="#" class="img-fluid">
+                    <h3><a href="single-product.php">Foam filling cotton slow rebound pillows</a></h3>
+                    <p>From $5</p>
                 </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <img src="img/tranding_item/tranding_item_2.png" alt="#" class="img-fluid">
-                        <h3> <a href="single-product.php">Foam filling cotton slow rebound pillows</a> </h3>
-                        <p>From $5</p>
-                    </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <img src="img/tranding_item/tranding_item_3.png" alt="#" class="img-fluid">
+                    <h3><a href="single-product.php">Memory foam filling cotton Slow rebound pillows</a></h3>
+                    <p>From $5</p>
                 </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <img src="img/tranding_item/tranding_item_3.png" alt="#" class="img-fluid">
-                        <h3> <a href="single-product.php">Memory foam filling cotton Slow rebound pillows</a> </h3>
-                        <p>From $5</p>
-                    </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <img src="img/tranding_item/tranding_item_4.png" alt="#" class="img-fluid">
+                    <h3><a href="single-product.php">Cervical pillow for airplane
+                            car office nap pillow</a></h3>
+                    <p>From $5</p>
                 </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <img src="img/tranding_item/tranding_item_4.png" alt="#" class="img-fluid">
-                        <h3> <a href="single-product.php">Cervical pillow for airplane
-                                car office nap pillow</a> </h3>
-                        <p>From $5</p>
-                    </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <img src="img/tranding_item/tranding_item_5.png" alt="#" class="img-fluid">
+                    <h3><a href="single-product.php">Foam filling cotton slow rebound pillows</a></h3>
+                    <p>From $5</p>
                 </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <img src="img/tranding_item/tranding_item_5.png" alt="#" class="img-fluid">
-                        <h3> <a href="single-product.php">Foam filling cotton slow rebound pillows</a> </h3>
-                        <p>From $5</p>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-sm-6">
-                    <div class="single_product_item">
-                        <img src="img/tranding_item/tranding_item_6.png" alt="#" class="img-fluid">
-                        <h3> <a href="single-product.php">Memory foam filling cotton Slow rebound pillows</a> </h3>
-                        <p>From $5</p>
-                    </div>
+            </div>
+            <div class="col-lg-4 col-sm-6">
+                <div class="single_product_item">
+                    <img src="img/tranding_item/tranding_item_6.png" alt="#" class="img-fluid">
+                    <h3><a href="single-product.php">Memory foam filling cotton Slow rebound pillows</a></h3>
+                    <p>From $5</p>
                 </div>
             </div>
         </div>
-    </section>
-    <!-- trending item end-->
+    </div>
+</section>
+<!-- trending item end-->
 
-   <!--::footer_part start::-->
-  <?php
-    require("footer.html");
-  ?>
-    <!--::footer_part end::-->
+<!--::footer_part start::-->
+<?php
+require("footer.html");
+?>
+<!--::footer_part end::-->
 
-    <!-- jquery plugins here-->
-    <script src="js/jquery-1.12.1.min.js"></script>
-    <!-- popper js -->
-    <script src="js/popper.min.js"></script>
-    <!-- bootstrap js -->
-    <script src="js/bootstrap.min.js"></script>
-    <!-- magnific popup js -->
-    <script src="js/jquery.magnific-popup.js"></script>
-    <!-- carousel js -->
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.nice-select.min.js"></script>
-    <!-- slick js -->
-    <script src="js/slick.min.js"></script>
-    <script src="js/jquery.counterup.min.js"></script>
-    <script src="js/waypoints.min.js"></script>
-    <script src="js/contact.js"></script>
-    <script src="js/jquery.ajaxchimp.min.js"></script>
-    <script src="js/jquery.form.js"></script>
-    <script src="js/jquery.validate.min.js"></script>
-    <script src="js/mail-script.js"></script>
-    <!-- custom js -->
-    <script src="js/custom.js"></script>
+<!-- jquery plugins here-->
+<script src="js/jquery-1.12.1.min.js"></script>
+<!-- popper js -->
+<script src="js/popper.min.js"></script>
+<!-- bootstrap js -->
+<script src="js/bootstrap.min.js"></script>
+<!-- magnific popup js -->
+<script src="js/jquery.magnific-popup.js"></script>
+<!-- carousel js -->
+<script src="js/owl.carousel.min.js"></script>
+<script src="js/jquery.nice-select.min.js"></script>
+<!-- slick js -->
+<script src="js/slick.min.js"></script>
+<script src="js/jquery.counterup.min.js"></script>
+<script src="js/waypoints.min.js"></script>
+<script src="js/contact.js"></script>
+<script src="js/jquery.ajaxchimp.min.js"></script>
+<script src="js/jquery.form.js"></script>
+<script src="js/jquery.validate.min.js"></script>
+<script src="js/mail-script.js"></script>
+<!-- custom js -->
+<script src="js/custom.js"></script>
 
 </div>
 </body>
-
 
 
 </html>
