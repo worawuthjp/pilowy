e67566c91ec327b19f022994507713dd83c3e34d
Gene013 <thanathorn.so@ku.th>
2020-07-05 00:43:03 +0700

    test session timeout

--- test.php
+++ test.php
@@ -1,11 +1,20 @@
 <html>
 <?php
+session_start();
+$id = $_SESSION['id'];
+$f_name = $_SESSION['f_name'];
+$l_name = $_SESSION['l_name'];
+$postal_code = $_SESSION['postal_code'];
+$email = $_SESSION['email'];
+$address = $_SESSION['address'];
+$phone = $_SESSION['phone'];
 
-$id = $_GET['id'];
-$phone = $_GET['phone'];
-
+echo session_id();
+echo "<br/>";
 print_r($id);
-
+echo "<br/>";
+print_r($f_name);
+echo "<br/>";
 print_r($phone);
 ?>
 
