38359b6faccf4dd76a096eae48e429f89583a6bf
parin1998 <60882177+parin1998@users.noreply.github.com>
2020-07-06 01:35:37 +0700

    edit login admin

--- admin-login.php
+++ admin-login.php
@@ -4,94 +4,31 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-        body {
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        form {
-            border: 3px solid #f1f1f1;
-        }
-
-        input[type=text],
-        input[type=password] {
-            width: 100%;
-            padding: 12px 20px;
-            margin: 8px 0;
-            display: inline-block;
-            border: 1px solid #ccc;
-            box-sizing: border-box;
-        }
-
-        button {
-            background-color: #4CAF50;
-            color: white;
-            padding: 14px 20px;
-            margin: 8px 0;
-            border: none;
-            cursor: pointer;
-            width: 100%;
-        }
-
-        button:hover {
-            opacity: 0.8;
-        }
-
-        .cancelbtn {
-            /* width: auto;
-            padding: 10px 18px; */
-            background-color: #f44336;
-        }
-
-        .imgcontainer {
+        .myDiv {
             text-align: center;
-            margin: 24px 0 12px 0;
-        }
-
-        img.avatar {
-            width: 40%;
-            border-radius: 50%;
-        }
-
-        .container {
-            padding: 16px;
-        }
-
-        span.psw {
-            float: right;
-            padding-top: 16px;
-        }
-
-        /* Change styles for span and cancel button on extra small screens */
-        @media screen and (max-width: 300px) {
-            span.psw {
-                display: block;
-                float: none;
-            }
-
-            .cancelbtn {
-                width: 100%;
-            }
         }
     </style>
 </head>
 
 <body>
+    <?php require("head.html"); ?>
 
-    <h2>Admin Login</h2>
+    <h2 align = 'center' >Admin Login</h2>
 
-    <form action="./checkuser.php" method="post">
+    <form class="row contact_form" action="./checkuser.php" method="post">
         <div class="container">
             <label for="uname"><b>Username</b></label>
-            <input type="text" placeholder="Enter Username" name="uname" required>
+            <input class="form-control" type="text" placeholder="Enter Username" name="uname" required>
 
             <label for="psw"><b>Password</b></label>
-            <input type="password" placeholder="Enter Password" name="psw" required>
+            <input class="form-control" type="password" placeholder="Enter Password" name="psw" required>
+            <br>
 
-            <button type="submit">Login</button>
         </div>
 
-        <div class="container" style="background-color:#f1f1f1">
-            <button type="button" class="cancelbtn">Cancel</button>
+        <div class="container myDiv" style="background-color:#f1f1f1">
+            <button class="btn_1 ml-auto mr-auto" type="submit">Login</button>
+            <button class="btn_3 ml-auto mr-auto" type="button">Cancel</button>
         </div>
     </form>
 
