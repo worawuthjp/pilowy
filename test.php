d8275b4e74a936bb68cbed612996687fff60e6af
Gene013 <thanathorn.so@ku.th>
2020-07-07 02:00:49 +0700

    test upload pic

--- test.php
+++ test.php
@@ -1,25 +1,5 @@
-<html>
 <?php
-session_start();
-$id = $_SESSION['id'];
-$f_name = $_SESSION['f_name'];
-$l_name = $_SESSION['l_name'];
-$postal_code = $_SESSION['postal_code'];
-$email = $_SESSION['email'];
-$address = $_SESSION['address'];
-$phone = $_SESSION['phone'];
-
-$product_id = $_POST['product_id'];
-$num = $_POST['num'];
-$price = $_POST['price'];
-
-echo session_id();
-echo "<br/>";
-print_r($product_id);
-echo "<br/>";
-print_r($num);
-echo "<br/>";
-print_r($price);
+if(isset($_POST['name']) && $_POST['city']!=""){
+    echo $_POST['name']; // ตัวอย่าง
+}
 ?>
-
-</html>
